e93e3fbfea2f88fbb507b663949419e930b308eb
Western8 <westernchrome@gmail.com>
2023-04-15 20:56:41 +0300

    feat: add function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options = {
+                'repeatTimes': 1,
+                'separator': '+',
+                'addition': '',
+                'additionRepeatTimes': 1,
+                'additionSeparator': '|',
+               }) {
+  let res = '';
+  let arr = [];
+  let arrAdd = [];
+
+  additionRepeatTimes = options.additionRepeatTimes || 1;
+  for (let k = 1; k <= additionRepeatTimes; k++) {
+    addition = options.addition || ( (typeof options.addition === 'undefined') ? '' : options.addition);
+    arrAdd.push(String(addition));
+  }
+  additionSeparator = options.additionSeparator || '|';
+  let strAdd = arrAdd.join(additionSeparator);
+
+  repeatTimes = options.repeatTimes || 1;
+  for (let k = 1; k <= repeatTimes; k++) {
+    arr.push(String(str) + String(strAdd));
+  }
+  separator = options.separator || '+';
+  res = arr.join(separator);
+
+  return res;
 }
 
 module.exports = {
